acc618bc5ac01dd0bf1313d4f7b0cf33582080d6
sirthxalot <sta.contribution@gmail.com>
2024-12-16 00:24:42 +0100

    Homepage
    Add fundamentals for homepage.

--- src/components/segments/HeaderSegment.vue
+++ src/components/segments/HeaderSegment.vue
@@ -14,33 +14,35 @@ function isHomepage() {
 <template>
   <el-sticky>
     <header id="top" class="header">
-      <div>
-        <menu-button class="button h-header w-[80px] text-4xl">
-          <i class="fal fa-bars"></i>
-        </menu-button>
-      </div>
-      <div class="ml-8 mr-auto">
-        <h1 class="site-name">
-          <template v-if="! isHomepage()">
-            <a href="/"
-               class="select-none"
-               title="Back to homepage"
-               v-html="siteTitle"
-            />
-          </template>
+      <div class="flex items-center container mx-auto">
+        <div>
+          <menu-button class="button h-header w-[80px] text-4xl">
+            <i class="fal fa-bars"></i>
+          </menu-button>
+        </div>
+        <div class="ml-8 mr-auto">
+          <h1 class="site-name">
+            <template v-if="! isHomepage()">
+              <a href="/"
+                 class="select-none"
+                 title="Back to homepage"
+                 v-html="siteTitle"
+              />
+            </template>
 
-          <template v-if="isHomepage()">
+            <template v-if="isHomepage()">
                 <span
                   class="cursor-default select-none"
 
                   v-html="siteTitle"
                 />
-          </template>
-        </h1>
+            </template>
+          </h1>
+        </div>
+        <nav aria-label="social menu" class="social hidden md:block mr-8 text-3xl">
+          <SocialMenu />
+        </nav>
       </div>
-      <nav aria-label="social menu" class="social hidden md:block mr-8 text-3xl">
-        <SocialMenu />
-      </nav>
     </header>
   </el-sticky>
 
--- src/views/pages/HomePage.vue
+++ src/views/pages/HomePage.vue
@@ -1,6 +1,43 @@
 <template>
   <page-layout>
-    <div>Homepage</div>
+    <div class="page page--home">
+      <section class="section section--container">
+        <div class="flex lg:w-[33%]">
+          <h3 class="headline--1 uppercase">Source of inspiration</h3>
+        </div>
+        <div class="flex lg:w-[66%] self-center text-2xl font-light">
+          <p>
+            Hi folks! My name is
+            <router-link to="/about" class="link">Alexander Bösch</router-link>
+            a.k.a. dippede doodle and I am your host.
+            Nice to meet you! This is my personal space where I share
+            everything related to art. Lets dive into the fantastic
+            world where creativity meets reality. The goal is
+            simple - spread out the inspirational spirit absorbed
+            due the artistic journey. Buckle up, be amazed and have fun.
+          </p>
+        </div>
+      </section>
+
+      <section class="section section--container">
+        <div class="flex lg:w-[66%] self-center text-2xl font-light">
+          <p>
+            Checkout
+            <router-link to="/portfolio" class="link">my portfolio</router-link>
+            jam-packed with tons of polished artwork. My gusto heavily
+            relies on penciled or inked artwork. Cartoon, comics
+            and street art is what get me goosebumps. There are
+            multiple ways to capture a moment and I love all of them.
+            Photos are another great example. Don't believe me?
+            Or do you want to see some? Than visit
+            <router-link to="/photos" class="link">my photo gallery</router-link>.
+          </p>
+        </div>
+        <div class="flex lg:w-[33%] text-right">
+          <h3 class="headline--1 uppercase">Fair representation of my skills</h3>
+        </div>
+      </section>
+    </div>
   </page-layout>
 </template>
 
